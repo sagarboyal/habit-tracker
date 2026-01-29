1284bb01de9ce956505df137b9a1151361541cf8
sagarboyal <sboyal12@gmail.com>
2026-01-29 17:51:21 +0530

    fix - extra params in primary button components

--- src/components/PrimaryButton/PrimaryButton.tsx
+++ src/components/PrimaryButton/PrimaryButton.tsx
@@ -4,8 +4,6 @@ import defaultStyles from "./PrimaryButton.styles";
 
 type PrimaryButtonProps = {
   text: string;
-  textColor?: string;
-  backgroundColor?: string;
   onPress?: () => void;
   style?: StyleProp<ViewStyle>;
   textStyle?: StyleProp<TextStyle>;
@@ -13,30 +11,13 @@ type PrimaryButtonProps = {
 
 const PrimaryButton = ({
   text,
-  textColor,
-  backgroundColor,
   style,
   textStyle,
   onPress,
 }: PrimaryButtonProps) => {
   return (
-    <Pressable
-      onPress={onPress}
-      style={[
-        defaultStyles.button,
-        backgroundColor ? { backgroundColor } : {},
-        style,
-      ]}
-    >
-      <Text
-        style={[
-          defaultStyles.buttonText,
-          textColor ? { color: textColor } : {},
-          textStyle,
-        ]}
-      >
-        {text}
-      </Text>
+    <Pressable onPress={onPress} style={[defaultStyles.button, style]}>
+      <Text style={[defaultStyles.buttonText, textStyle]}>{text}</Text>
     </Pressable>
   );
 };
